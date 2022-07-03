fbcfef3e87ce822816cbc4d40aaa0d0debf02e03
Stilic <lateman-jpeg@outlook.fr>
2022-07-03 20:40:33 +0200

    bruh

--- main.lua
+++ main.lua
@@ -7,12 +7,9 @@ paths = require "game.paths"
 local music
 
 local title = require "game.states.title"
-
 local curState
 
-function callState(func, ...)
-    if curState[func] then curState[func](...) end
-end
+function callState(func, ...) if curState[func] then curState[func](...) end end
 
 function switchState(state)
     curState = state
@@ -22,9 +19,11 @@ end
 
 function love.load()
     lovesize.set(love.graphics.getDimensions())
+
     music = lovebpm.newTrack():load(paths.musicPath("freakyMenu")):setBPM(102)
                 :setLooping(true)
                 :on("beat", function(n) callState("beat", n) end)
+
     switchState(title)
     music:play()
 end
@@ -33,6 +32,7 @@ function love.resize(width, height) lovesize.resize(width, height) end
 
 function love.update(dt)
     dt = math.min(dt, 1 / 60)
+
     music:update(dt)
     callState("update", dt)
 end
